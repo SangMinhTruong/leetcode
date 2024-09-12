2cc5e8258afece5cfced9a513e219223a7151486
Sang Truong <minhsangtruong1999@gmail.com>
2024-09-12 23:21:49 +0700

    Add 206. Reverse Linked List

---
+++ tech-interview-handbook/Linked List/206.ReverseLinkedList.cs
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        return ReverseListIterative(head);
+    }
+
+    private ListNode ReverseListIterative(ListNode head) {
+        ListNode newHead = null;
+        while (head != null) {
+            var next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
+        }
+        return newHead;
+    }
+
+    private ListNode ReverseListRecurisve(ListNode head, ListNode newHead = null) {
+        if (head == null) {
+            return newHead;
+        }
+        var next = head.next;
+        head.next = newHead;
+        return ReverseListRecurisve(next, head);
+    }
+}
