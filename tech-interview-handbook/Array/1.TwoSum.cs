7efa082c300e96fd559d2e51231a58fba3e7cb62
Sang Truong <minhsangtruong1999@gmail.com>
2024-09-16 20:38:31 +0700

    Add 1. Two Sum

---
+++ tech-interview-handbook/Array/1.TwoSum.cs
@@ -0,0 +1,17 @@
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var dictionary = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var numToFind = target - nums[i];
+            if (dictionary.ContainsKey(numToFind))
+            {
+                return new int[2] { i, dictionary[numToFind] };
+            }
+            dictionary[nums[i]] = i;
+        }
+        return new int[2];
+    }
+}
