0a88811d3bbdef8c73a3e1a1729dfdb8e147b00c
Sang <minhsangtruong1999@gmail.com>
2025-10-16 11:34:04 +0700

    Update 56. Merge Intervals

--- tech-interview-handbook/Interval/56.MergeIntervals.cs
+++ tech-interview-handbook/Interval/56.MergeIntervals.cs
@@ -4,18 +4,13 @@ public class Solution
     {
         Array.Sort(intervals, (x, y) => x[0].CompareTo(y[0]));
 
-        List<int[]> result = new List<int[]>();
-        result.Add(intervals[0]);
+        List<int[]> result = new List<int[]>() { intervals[0] };
         foreach (var interval in intervals)
         {
             if (interval[0] <= result[result.Count - 1][1])
-            {
                 result[result.Count - 1][1] = Math.Max(interval[1], result[result.Count - 1][1]);
-            }
             else
-            {
                 result.Add(interval);
-            }
         }
 
         return result.ToArray();
