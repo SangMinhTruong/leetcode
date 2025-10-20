ab3417c249a80961119cff078d253073c9328e64
SangMinhTruong <minhsangtruong1999@gmail.com>
2025-10-20 16:17:09 +0700

    Update 542. 01 Matrix

--- tech-interview-handbook/Graph/542.01Matrix.cs
+++ tech-interview-handbook/Graph/542.01Matrix.cs
@@ -1,84 +1,88 @@
 public class Solution
 {
-  int[] DIR = new int[] { 0, 1, 0, -1, 0 };
-
-  public int[][] UpdateMatrix(int[][] mat)
-  {
-    return UpdateMatrixDP(mat);
-  }
-
-  private int[][] UpdateMatrixBFS(int[][] mat)
-  {
-    int m = mat.Length;
-    int n = mat[0].Length;
-
-    Queue<int[]> queue = new Queue<int[]>();
-    for (int i = 0; i < m; i++)
+    public int[][] UpdateMatrix(int[][] mat)
     {
-      for (int j = 0; j < n; j++)
-      {
-        if (mat[i][j] == 0)
-          queue.Enqueue(new int[] { i, j });
-        else
-          mat[i][j] = -1;
-      }
+        return UpdateMatrixDP(mat);
     }
 
-    while (queue.Count > 0)
+    private int[][] UpdateMatrixBFS(int[][] mat)
     {
-      int[] cur = queue.Dequeue();
-      int r = cur[0], c = cur[1];
-      for (int i = 0; i < 4; i++)
-      {
-        int nr = r + DIR[i];
-        int nc = c + DIR[i + 1];
-        if (nr < 0 || nc < 0 || nr >= m || nc >= n || mat[nr][nc] != -1)
-          continue;
-        mat[nr][nc] = mat[r][c] + 1;
-        queue.Enqueue(new int[] { nr, nc });
-      }
+        int m = mat.Length;
+        int n = mat[0].Length;
+        Queue<(int, int)> queue = new Queue<(int, int)>();
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (mat[i][j] == 0)
+                    queue.Enqueue((i, j));
+                else
+                    mat[i][j] = -1;
+            }
+        }
+
+        (int, int)[] directions = new (int, int)[] { (1, 0), (-1, 0), (0, 1), (0, -1) };
+        while (queue.Count > 0)
+        {
+            (int i, int j) = queue.Dequeue();
+            foreach ((int dirI, int dirJ) in directions)
+            {
+                int newI = i + dirI;
+                int newJ = j + dirJ;
+
+                if (newI < 0 || newJ < 0 || newI >= m || newJ >= n || mat[newI][newJ] != -1)
+                    continue;
+
+                mat[newI][newJ] = mat[i][j] + 1;
+                queue.Enqueue((newI, newJ));
+            }
+        }
+
+        return mat;
     }
 
-    return mat;
-  }
-
-  private int[][] UpdateMatrixDP(int[][] mat)
-  {
-    int m = mat.Length;
-    int n = mat[0].Length;
-    int MAX = m + n;
-    for (int i = 0; i < m; i++)
+    private int[][] UpdateMatrixDP(int[][] mat)
     {
-      for (int j = 0; j < n; j++)
-      {
-        if (mat[i][j] == 0)
-          continue;
+        int m = mat.Length;
+        int n = mat[0].Length;
+        int MAX = m + n;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (mat[i][j] == 0)
+                    continue;
 
-        int top = MAX, left = MAX;
-        if (i - 1 >= 0)
-          top = mat[i - 1][j];
-        if (j - 1 >= 0)
-          left = mat[i][j - 1];
-        mat[i][j] = Math.Min(top, left) + 1;
-      }
+                int top = MAX;
+                int left = MAX;
+                if (i - 1 >= 0)
+                    top = mat[i - 1][j];
+                if (j - 1 >= 0)
+                    left = mat[i][j - 1];
+
+                mat[i][j] = Math.Min(top, left) + 1;
+            }
+        }
+
+        for (int i = m - 1; i >= 0; i--)
+        {
+            for (int j = n - 1; j >= 0; j--)
+            {
+                if (mat[i][j] == 0)
+                    continue;
+
+                int bottom = MAX;
+                int right = MAX;
+                if (i + 1 < m)
+                    bottom = mat[i + 1][j];
+                if (j + 1 < n)
+                    right = mat[i][j + 1];
+
+                mat[i][j] = Math.Min(mat[i][j], Math.Min(bottom, right) + 1);
+            }
+        }
+
+        return mat;
     }
-
-    for (int i = m - 1; i >= 0; i--)
-    {
-      for (int j = n - 1; j >= 0; j--)
-      {
-        if (mat[i][j] == 0)
-          continue;
-
-        int bottom = MAX, right = MAX;
-        if (i + 1 < m)
-          bottom = mat[i + 1][j];
-        if (j + 1 < n)
-          right = mat[i][j + 1];
-        mat[i][j] = Math.Min(mat[i][j], Math.Min(bottom, right) + 1);
-      }
-    }
-
-    return mat;
-  }
 }
