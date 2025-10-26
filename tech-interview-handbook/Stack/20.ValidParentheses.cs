4c0bf8ed9e69fa3a010ff29fa2ef66f5368157d9
SangMinhTruong <minhsangtruong1999@gmail.com>
2025-10-26 11:06:20 +0700

    Update formatting

--- tech-interview-handbook/Stack/20.ValidParentheses.cs
+++ tech-interview-handbook/Stack/20.ValidParentheses.cs
@@ -3,7 +3,8 @@ public class Solution
     public bool IsValid(string s)
     {
         Stack<char> stack = new Stack<char>();
-        foreach (var c in s)
+
+        foreach (char c in s)
         {
             if (c == '(')
                 stack.Push(')');
@@ -11,9 +12,10 @@ public class Solution
                 stack.Push('}');
             else if (c == '[')
                 stack.Push(']');
-            else if (stack.Count == 0 || stack.Pop() != c)
+            else if (stack.Count <= 0 || stack.Pop() != c)
                 return false;
         }
+
         return stack.Count == 0;
     }
 }
