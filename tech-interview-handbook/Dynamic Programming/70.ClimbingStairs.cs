83f67fd601137b8184d7ebf5cd49ddf43759496c
SangMinhTruong <minhsangtruong1999@gmail.com>
2025-10-26 14:11:17 +0700

    Update formatting

--- tech-interview-handbook/Dynamic Programming/70.ClimbingStairs.cs
+++ tech-interview-handbook/Dynamic Programming/70.ClimbingStairs.cs
@@ -11,6 +11,7 @@ public class Solution
     {
         if (n == 0 || n == 1)
             return 1;
+
         return ClimbStairsRecursion(n - 1) + ClimbStairsRecursion(n - 2);
     }
 
@@ -18,8 +19,10 @@ public class Solution
     {
         if (n == 0 || n == 1)
             return 1;
+
         if (!memo.ContainsKey(n))
             memo.Add(n, ClimbStairsMemo(n - 1, memo) + ClimbStairsMemo(n - 2, memo));
+
         return memo[n];
     }
 
@@ -27,8 +30,10 @@ public class Solution
     {
         int[] dp = new int[n + 1];
         dp[0] = dp[1] = 1;
+
         for (int i = 2; i <= n; i++)
             dp[i] = dp[i - 1] + dp[i - 2];
+
         return dp[n];
     }
 
@@ -37,12 +42,14 @@ public class Solution
         int first = 1;
         int second = 1;
         int result = first;
+
         for (int i = 2; i <= n; i++)
         {
             result = first + second;
             second = first;
             first = result;
         }
+
         return result;
     }
 }
