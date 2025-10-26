513d3dfa543ff17e9c289d64aaa5a13f6f31a00a
SangMinhTruong <minhsangtruong1999@gmail.com>
2025-10-26 13:44:22 +0700

    Add 232. Implement Queue using Stacks

---
+++ grind-75/232.ImplementQueueUsingStacks.cs
@@ -0,0 +1,49 @@
+public class MyQueue
+{
+    private Stack<int> input;
+    private Stack<int> output;
+
+    public MyQueue()
+    {
+        input = new Stack<int>();
+        output = new Stack<int>();
+    }
+
+    public void Push(int x)
+    {
+        input.Push(x);
+    }
+
+    public int Pop()
+    {
+        Peek();
+        return output.Pop();
+    }
+
+    public int Peek()
+    {
+        if (output.Count == 0)
+        {
+            while (input.Count > 0)
+            {
+                output.Push(input.Pop());
+            }
+        }
+
+        return output.Peek();
+    }
+
+    public bool Empty()
+    {
+        return input.Count == 0 && output.Count == 0;
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.Push(x);
+ * int param_2 = obj.Pop();
+ * int param_3 = obj.Peek();
+ * bool param_4 = obj.Empty();
+ */
