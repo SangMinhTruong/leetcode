5b73212a8898ec00b9db1bd8181ac6c99f2add7e
Sang <minhsangtruong1999@gmail.com>
2025-11-11 13:09:07 +0700

    Update 191. Number of 1 Bits

--- tech-interview-handbook/Binary/191.NumberOf1Bits.cs
+++ tech-interview-handbook/Binary/191.NumberOf1Bits.cs
@@ -2,19 +2,40 @@ public class Solution
 {
     public int HammingWeight(int n)
     {
+        return HammingWeightBitFlip(n);
+    }
+
+    private int HammingWeightBitShift(int n)
+    {
         int count = 0;
 
-        // 1011 n
-        // 1010 n - 1
-        // 1010 n & (n - 1)
+        while (n != 0)
+        {
+            if ((n & 1) == 1)
+                count++;
+
+            n >>= 1;
+        }
+
+        return count;
+    }
+
+    private int HammingWeightBitFlip(int n)
+    {
+        int count = 0;
+
+        // n: ...100 1 00... <- ALL 0s -> ...000
+        //           ^
+        // the first 1 bit starting from the right
         //
-        // 1010 n
-        // 1001 n - 1
-        // 1000 n & (n - 1)
+        // n - 1: ...100 0 11... <- ALL 1s -> ...111
+        //               ^
+        // the first 1 bit becomes 0 and ALL the 0s to the right become 1s
         //
-        // 1000 n
-        // 0111 n - 1
-        // 0000 n & (n - 1)
+        // n & (n - 1): ...100 0 00... <- ALL 0s -> ...000
+        //                     ^
+        // the 1 bit is flipped and considered counted, ALL bits to the right is set to 0s
+        // we then repeat the process until there is no more 1 bits
 
         while (n != 0)
         {
