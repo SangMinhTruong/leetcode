854a5586edeca4d3553b6b3d33f8e0f038c39ccf
SangMinhTruong <minhsangtruong1999@gmail.com>
2025-11-30 14:12:32 +0700

    Update 198. House Robber

--- tech-interview-handbook/Dynamic Programming/198.HouseRobber.cs
+++ tech-interview-handbook/Dynamic Programming/198.HouseRobber.cs
@@ -23,19 +23,16 @@ public class Solution
 
     private int RobIterative(int[] nums)
     {
-        if (nums.Length == 0)
-            return 0;
-
-        int prev1 = 0;
-        int prev2 = 0;
+        int lastHouse = 0;
+        int lastOtherHouse = 0;
 
         foreach (int num in nums)
         {
-            int cur = Math.Max(prev1 + num, prev2);
-            prev1 = prev2;
-            prev2 = cur;
+            int curHouse = Math.Max(lastOtherHouse + num, lastHouse);
+            lastOtherHouse = lastHouse;
+            lastHouse = curHouse;
         }
 
-        return prev2;
+        return lastHouse;
     }
 }
